c267f02a81d93fec423916789b977d1fbb65fec1
koallcia <alicja.konkol@gmail.com>
2017-12-11 23:14:31 +0100

    Continue of AStar Algorithm Implementation

--- app/src/main/java/com/geoida/progeo/RouteGenerating/AStar/AStarAlgorithm.java
+++ app/src/main/java/com/geoida/progeo/RouteGenerating/AStar/AStarAlgorithm.java
@@ -3,7 +3,9 @@ package com.geoida.progeo.RouteGenerating.AStar;
 import com.geoida.progeo.RouteGenerating.Dijkstra.Graph;
 import com.geoida.progeo.RouteGenerating.Dijkstra.GraphEdge;
 import com.geoida.progeo.RouteGenerating.Dijkstra.GraphVertex;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
@@ -37,7 +39,16 @@ public class AStarAlgorithm {
         return min;
     }
 
-    private List<GraphVertex> reconstructPath(GraphVertex endV){};
+    private List<GraphVertex> reconstructPath(GraphVertex startV, GraphVertex endV){
+        List<GraphVertex> path;
+        path = new ArrayList<GraphVertex>();
+        path.add(endV);
+        while (!path.contains(startV)){
+            path.add(predecessors.get(endV));
+            endV = predecessors.get(endV);
+        }
+        return path;
+    }
 
     public List<GraphVertex> compute(GraphVertex startVertex, GraphVertex endVertex){
         startVertex.changeCost(0);
@@ -46,7 +57,7 @@ public class AStarAlgorithm {
         while(!notVisited.isEmpty()){
             GraphVertex x = findMin();
             if( x.equals(endVertex)){
-                return reconstructPath(endVertex);
+                return reconstructPath(startVertex, endVertex);
             }
             notVisited.remove(x);
             visited.add(x);
@@ -54,12 +65,25 @@ public class AStarAlgorithm {
                 if(visited.contains(y)){
                     continue;
                 }
-                //Double tentativeScore = x.getCost() + TODO create heuristic values
-
+                Double tentativeScore = x.getCost(); // + function TODO by Kamcio
+                Boolean tentativeBetter = false;
+                if (!notVisited.contains(y)){
+                    notVisited.add(y);
+                    //h_score[y] := heuristic_estimate_of_distance_to_goal_from(y) TODO by Kamcio
+                    tentativeBetter = true;
+                }
+                else if(tentativeScore < y.getCost()){
+                    tentativeBetter = true;
+                }
+                if(tentativeBetter == true){
+                    predecessors.put(y, x);
+                    y.changeCost(tentativeScore);
+                    //f_score[y] := g_score[y] + h_score[y] waiting for Kamcio
+                }
             }
 
-
         }
+        return null;
     }
 
 
